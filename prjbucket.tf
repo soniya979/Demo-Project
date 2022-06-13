420d8c34fce64e02028788bfafe88ca3887bc994
soniya979 <soniyapatel979@gmail.com>
2022-06-13 23:32:43 +0100

    Create prjbucket.tf

---
+++ prjbucket.tf
@@ -0,0 +1,37 @@
+#create s3 bucket for project01
+
+resource "aws_s3_bucket" "project01-s3-data-bucket" {
+
+   bucket = "project01-s3-data-bucket"
+
+  }
+
+resource "aws_s3_bucket_acl" "s3-raw-acl" {
+
+    bucket = aws_s3_bucket.project01-s3-data-bucket.id
+
+    acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "s3-raw-data-versioning" {
+
+  bucket = aws_s3_bucket.project01-s3-data-bucket.id
+
+  versioning_configuration {
+
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3-demo-encyption" {
+
+  bucket = aws_s3_bucket.project01-s3-data-bucket.bucket
+
+  rule {
+
+    apply_server_side_encryption_by_default {
+
+      sse_algorithm     = "AES256"
+    }
+  }
+}
