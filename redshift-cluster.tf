169e05ddfce2d5880940c9683a666b646e19b8fb
soniya979 <soniyapatel979@gmail.com>
2022-06-14 22:41:07 +0100

    Update redshift-cluster.tf

--- redshift-cluster.tf
+++ redshift-cluster.tf
@@ -1,17 +1,17 @@
 # launch Redshift_cluster select region same as emr
 
-#attach policy awsquery editor, aws readonly redhshift to iam user.
+#attach polici awsquery editor, aws readonly redhsift to iam user.
 
 resource "aws_redshift_cluster" "redshift-cluster-01" {
   cluster_identifier = "redshift-cluster-01"
-  database_name      = "demo-rds-db"
+  database_name      = "demordsdb"
   master_username    = "demordsuser"
   master_password    = "Demo$12345"
   node_type          = "dc2.large"
   cluster_type       = "single-node"
   cluster_subnet_group_name = aws_redshift_subnet_group.redshift_subnet_group.id
 
-  iam_roles = aws_iam_role.redshift_role.arn
+  iam_roles = [aws_iam_role.redshift_role.arn]
 
 depends_on = [
     aws_vpc.redshift_vpc,
