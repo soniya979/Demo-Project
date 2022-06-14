5564cc7e50bf030625ef00b49b9de68dc5f3552e
soniya979 <soniyapatel979@gmail.com>
2022-06-14 22:51:32 +0100

    Update redshift-cluster.tf

--- redshift-cluster.tf
+++ redshift-cluster.tf
@@ -15,7 +15,7 @@ resource "aws_redshift_cluster" "redshift-cluster-01" {
 
 depends_on = [
     aws_vpc.redshift_vpc,
-    aws_security_group.redshift_security_group,
+    aws_default_security_group.redshift_security_group,
     aws_redshift_subnet_group.redshift_subnet_group,
     aws_iam_role.redshift_role
   ]
